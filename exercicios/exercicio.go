0e23f79a6a93b02762ebe58d9156ffffb248650a
Josh7GAS <joshuaandre@gmail.com>
2019-04-16 00:10:28 -0300

    Atualizando exercicios

--- exercicios/exercicio.go
+++ exercicios/exercicio.go
@@ -6,6 +6,18 @@ func soma(A int, B int) int {
 	return A + B
 }
 
+//le numero e devolve
+func lernumero() float64 {
+	fmt.Println("Entre um número: ")
+	var x float64
+	fmt.Scanf("%f", &x)
+	if x < 0 {
+		x = x * (-1)
+	}
+	return x
+}
+
 func main() {
-	fmt.Println("Soma é %+v\r\n", soma(34, 88))
+	fmt.Println("Soma é ", soma(34, 88))
+	fmt.Println("Numero é ", lernumero())
 }

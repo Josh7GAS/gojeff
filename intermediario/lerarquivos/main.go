6212c80202ad8db888734a927cb62af415862824
Joshua <joshuaandre@gmail.com>
2019-04-17 14:38:46 -0300

    lerarquivos, defer, escreverarquivos, goroutines

--- intermediario/lerarquivos/main.go
+++ intermediario/lerarquivos/main.go
@@ -1,71 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
-	"strings"
-
-	"github.com/Josh7GAS/gojeff/intermediario/lerarquivos/model"
-) 
+)
 
 func main() {
-	arquivo, err := os.Open("cidades.cvs")
+	arquivo, err := os.Open("cidades.csv")
 	if err != nil {
-		fmt.Println("[main] Houve um erro ao abrir ao arquivo. Erro: ", err.Error())
+		fmt.Println("[main]Houve um erro ao abrir o arquivo CSV. Erro: ", err.Error())
 		return
 	}
-
-
 
 	// scanner := bufio.NewScanner(arquivo)
 	// for scanner.Scan() {
-	// 	linha := scanner.Text
-	// 	fmt.Println("O Conteúdo da linha é: ", linha())
+	// 	linha := scanner.Text()
+	// 	fmt.Println("O conteúdo da linha é: ", linha)
 	// }
-
 	leitorCsv := csv.NewReader(arquivo)
 	conteudo, err := leitorCsv.ReadAll()
 	if err != nil {
-		fmt.Println("[main] Houve um erro ao ler o arquivo com leitor CSV. Error: ",err.Error())
+		fmt.Println("[main] Houve um erro ao ler o arquivo com leitor CSV. Erro: ", err.Error())
 		return
 	}
-
-	arquivoJson, err := os.Create("cidades.json")
-	if err != nil {
-		fmt.Println("[main] Houve um erro ao criar o arquivo JSON. Erro: ", err.Error())
-		return
-	}
-
-
-	escritor := bufio.NewWriter(arquivoJson)
-	escritor.WriteString("[\r\n")
-
-	for _, linha := range conteudo {
+	for indiceLinha, linha := range conteudo {
+		fmt.Printf("Linha [%d] é %s\r\n", indiceLinha, linha)
 		for indiceItem, item := range linha {
-			dados := strings.Split(item, "/")
-			cidade := model.Cidade{}
-			cidade.Nome:=dados[0]
-			cidade.Estado:=dados[1]
-			fmt.Printf("Cidade: %+v\r\n", cidade)
-
-			cidadeJSON:=json.Marshal(cidade)
-
-			if err != nil {
-				fmt.Println("[main] Houve um erro ao gerar o json do item",item, ".Erro: ", err.Error() )
-				
-			}
-			escritor.WriteString(" " + string(cidadeJSON))
-			if (indiceItem+1) < len(linha){
-				escritor.WriteString(",\r\n")
-			}
-		 
+			fmt.Printf("Item[%d] é %s\r\n", indiceItem, item)
 		}
 	}
-	escritor.WriteString("\r\n]")
-	escritor.Flush()
-	arquivo.Json.Close()
 
 	arquivo.Close()
 }

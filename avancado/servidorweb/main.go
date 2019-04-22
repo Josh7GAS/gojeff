b998510d540c5d5bcb9fabe47a1cd5c022f4636d
Joshua <joshuaandre@gmail.com>
2019-04-22 09:48:19 -0300

    servidorweb

--- avancado/servidorweb/main.go
+++ avancado/servidorweb/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Josh7GAS/gojeff/avancado/servidorweb/manipulador"
 )
 
 func main() {
---
+++ avancado/servidorweb/model/pagina.go
@@ -0,0 +1,6 @@
+package model
+
+//pagina armazena as informações a serem renderizadas no modelo/template http Go
+type Pagina struct {
+	Hora string
+}

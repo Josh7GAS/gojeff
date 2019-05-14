d2d1185071fcbd3332b957dbe402d0c5fbb868f9
joshua <joshua@nubeliu.com>
2019-05-14 15:02:54 -0300

    bdconnect

---
+++ bdconnect/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/ini.v1"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:senha@/testedb")
+	if err != nil {
+		fmt.Println("Deu Erro",
+			err.Error())
+		return
+
+	} else {
+		fmt.Println("Conxão Estabelecida!!!!")
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Não conseguiu pingar", err.Error())
+	}
+
+	arquivo, err := ini.Load(os.Getenv("HOME") + "/.my.cnf")
+	if err != nil {
+		fmt.Printf("Não foi possível ler o aquivo: %v", err)
+		os.Exit(1)
+	} else {
+
+		// lendoArquivo, err := arquivo.Section("client").Key("hosts").Validate(func(in string) string {
+		// 	if len(in) == 0 {
+		// 		return "default"
+		// 	}
+		// 	return in
+		// })
+
+		fmt.Println("host:", arquivo.Section("client").Key("host").String())
+		fmt.Println("user:", arquivo.Section("client").Key("port").String())
+		fmt.Println("password:", arquivo.Section("client").Key("password").String())
+	}
+}
+
+// [client]
+// host=127.0.0.1
+// port=3306
+// user=root
+// password=senha

4b97a2219d9086d2a7a6df4dd7f7c2c0c3044bf4
joshua <joshua@nubeliu.com>
2019-05-17 14:55:11 -0300

    continuação, fazer json

--- bdconnect/main.go
+++ bdconnect/main.go
@@ -10,38 +10,27 @@ import (
 )
 
 func main() {
-	//credencial := cfgArquivo() Array vinda da função
-	credencial := cfgArquivo()
 
-	//variaveis que alimentam as index da array credencial
-	// host := credencial[0]
-	// port := credencial[1]
+	credencial := cfgArquivo()
+	host := credencial[0]
+	port := credencial[1]
 	user := credencial[2]
 	password := credencial[3]
+	var (
+		keys, values string
+	)
 
-	//conctando o banco de dados
-	db, _ := sql.Open("mysql", user+":"+password+"@/testedb")
-	if db != nil {
-		//caso bem sucedido ira fazer o globalstatus e armazenar em um slice
+	db, _ := sql.Open("mysql", user+":"+password+"@tcp"+"("+host+":"+port+")/testedb")
 
-		//trabalhe aqui com (foor range loop), (Next()), (Ponteiros)
-		mostraGlobalStatus, _ := db.Query("SHOW GLOBAL STATUS;")
-		if mostraGlobalStatus != nil {
-			slice1 := make([]*sql.Rows, 20)
-			slice1 = append(slice1, mostraGlobalStatus)
-			for _, v := range slice1 {
-				fmt.Println(v)
-			}
-		}
+	mostraGlobalStatus, _ := db.Query("SHOW GLOBAL STATUS;")
 
+	for mostraGlobalStatus.Next() {
+		mostraGlobalStatus.Scan(&keys, &values)
+		fmt.Println(keys, values)
 	}
-
-	defer db.Close()
 
 }
 
-//função para avbrir o arquivo .my.cnf e procurar as informações das variaveis, elas são armazenadas na array credencial []string and
-//e são usadad mais tarde quando essa função devolver a array
 func cfgArquivo() []string {
 	var (
 		credencial []string

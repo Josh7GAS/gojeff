a781219cfb6e6659aeab97f30127e4363d12e749
Josh7GAS <joshuaandre@gmail.com>
2019-05-19 16:06:02 -0300

    pacotes exercicio

--- fundamentos/packages/operadora/nome.go
+++ fundamentos/packages/operadora/nome.go
@@ -1,4 +1,13 @@
 package operadora
 
+import (
+	"strconv"
+
+	"github.com/Josh7GAS/gojeff/fundamentos/packages/prefixos"
+)
+
 //NomeOperadora é o nome da operadora
 var NomeOperadora = "XPTO Telecom"
+
+//PrefixoOperadora prefixo mais o nome da operadora
+var PrefixoOperadora = strconv.Itoa(prefixos.Capital) + "" + NomeOperadora
--- fundamentos/packages/prefixos/numeros.go
+++ fundamentos/packages/prefixos/numeros.go
@@ -1,4 +1,13 @@
 package prefixos
 
+import (
+	"strconv"
+)
+
 //Capital representa o numero de prefixo de telefone da capital de um estado/provencia
 var Capital = 11
+
+var teste = "teste"
+
+//TesteComPrefixo isto é so  um teste
+var TesteComPrefixo = teste + " " + strconv.Itoa(Capital)
